3e09d4e803ae96b718abadbd0635f79aa91fac22
alif <aliftareq@gmail.com>
2024-12-22 09:13:30 +0600

    created custom parallax cover

--- bistro-boss-client/src/pages/Menu/Menu/Menu.jsx
+++ bistro-boss-client/src/pages/Menu/Menu/Menu.jsx
@@ -1,11 +1,38 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../shared/Cover/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg";
+import dessertImg from "../../../assets/menu/dessert-bg.jpeg";
+import pizzaImg from "../../../assets/menu/pizza-bg.jpg";
+import saladImg from "../../../assets/menu/salad-bg.jpg";
+import soupImg from "../../../assets/menu/soup-bg.jpg";
+
+import PopulerMenu from "../../Home/PopulerMenu/PopulerMenu";
 const Menu = () => {
   return (
     <div>
       <Helmet>
         <title>BISTRO | MENU</title>
       </Helmet>
-      <h2>Menu page</h2>
+      <Cover img={menuImg} title="Our Menu"></Cover>
+      <div className="mx-20">
+        <PopulerMenu />
+      </div>
+      <Cover img={dessertImg} title="Our Menu"></Cover>
+      <div className="mx-20">
+        <PopulerMenu />
+      </div>
+      <Cover img={pizzaImg} title="Our Menu"></Cover>
+      <div className="mx-20">
+        <PopulerMenu />
+      </div>
+      <Cover img={saladImg} title="Our Menu"></Cover>
+      <div className="mx-20">
+        <PopulerMenu />
+      </div>
+      <Cover img={soupImg} title="Our Menu"></Cover>
+      <div className="mx-20">
+        <PopulerMenu />
+      </div>
     </div>
   );
 };
