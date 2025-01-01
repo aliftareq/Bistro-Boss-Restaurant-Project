6281028f515047e8f7294b393c77ff4610d00226
alif <aliftareq@gmail.com>
2025-01-01 18:42:28 +0600

    order page upgraded

--- bistro-boss-client/src/pages/Order/OrderTab/OrderTab.jsx
+++ bistro-boss-client/src/pages/Order/OrderTab/OrderTab.jsx
@@ -2,10 +2,12 @@ import FoodCard from "../../../components/FoodCard/FoodCard";
 
 const OrderTab = ({ items }) => {
   return (
-    <div className="grid md:grid-cols-3 gap-4 mx-10 my-5">
-      {items.map((item) => (
-        <FoodCard key={item._id} item={item}></FoodCard>
-      ))}
+    <div className="flex justify-center items-end">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 mx-10 my-4">
+        {items.map((item) => (
+          <FoodCard key={item._id} item={item}></FoodCard>
+        ))}
+      </div>
     </div>
   );
 };
